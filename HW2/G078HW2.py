972a59d5839fe68fa08ce623b27f20e7696e527d
danielcarlesso <daniel.carlesso@studenti.unipd.it>
2023-05-15 13:16:30 +0200

    reorganization of directories

---
+++ HW2/G078HW2.py
@@ -0,0 +1,220 @@
+import array
+from xml.dom.minidom import Identified
+from pyspark import SparkContext, SparkConf
+from collections import defaultdict
+import sys
+import os
+import random
+import time
+
+
+
+#Triangles counter in a group of edges
+def CountTriangles(edges):
+    # Create a defaultdict to store the neighbors of each vertex
+    neighbors = defaultdict(set)
+    for edge in edges:
+        u, v = edge
+        neighbors[u].add(v)
+        neighbors[v].add(u)
+
+    # Initialize the triangle count to zero
+    triangle_count = 0
+
+    # Iterate over each vertex in the graph.
+    # To avoid duplicates, we count a triangle <u, v, w> only if u<v<w
+    for u in neighbors:
+        # Iterate over each pair of neighbors of u
+        for v in neighbors[u]:
+            if v > u:
+                for w in neighbors[v]:
+                    # If w is also a neighbor of u, then we have a triangle
+                    if w > v and w in neighbors[u]:
+                        triangle_count += 1
+    # Return the total number of triangles in the graph
+    return triangle_count
+
+
+
+#Triangles counter
+def countTriangles2(colors_tuple, edges, rand_a, rand_b, p, num_colors):
+    #We assume colors_tuple to be already sorted by increasing colors. Just transform in a list for simplicity
+    colors = list(colors_tuple)  
+    #Create a dictionary for adjacency list
+    neighbors = defaultdict(set)
+    #Creare a dictionary for storing node colors
+    node_colors = dict()
+    for edge in edges:
+
+        u, v = edge
+        node_colors[u]= ((rand_a*u+rand_b)%p)%num_colors
+        node_colors[v]= ((rand_a*v+rand_b)%p)%num_colors
+        neighbors[u].add(v)
+        neighbors[v].add(u)
+
+    # Initialize the triangle count to zero
+    triangle_count = 0
+
+    # Iterate over each vertex in the graph
+    for v in neighbors:
+        # Iterate over each pair of neighbors of v
+        for u in neighbors[v]:
+            if u > v:
+                for w in neighbors[u]:
+                    # If w is also a neighbor of v, then we have a triangle
+                    if w > u and w in neighbors[v]:
+                        # Sort colors by increasing values
+                        triangle_colors = sorted((node_colors[u], node_colors[v], node_colors[w]))
+                        # If triangle has the right colors, count it.
+                        if colors==triangle_colors:
+                            triangle_count += 1
+    # Return the total number of triangles in the graph
+    return triangle_count
+
+
+
+#ALGORITHM 1
+#input:
+#		edges - RDD with the edges
+#		C - number of colors
+#output:
+#		t_final - estimate of the number of triangles
+def MR_ApproxTCwithNodeColors(edges, C=1):
+	p = 8191
+	a = random.randint(1, p-1)
+	b = random.randint(0, p-1)
+
+	#input:
+	#		e - edge
+	#output:
+	#		(color,edge): color = -1 if non monochromatical edge
+	def hash(e):
+		h1 = ((a * e[0] + b) % p) % C
+		h2 = ((a * e[1] + b) % p) % C
+
+		if (h1 == h2):
+			return (h1, (e[0], e[1]))
+		return (-1, (e[0], e[1]))
+
+	#ROUND 1	
+	rdd = (edges.map(hash).filter(lambda x: x[0] != -1)		#MAP PHASE
+		.groupByKey()										#SHUFFLE PHASE	
+		.mapValues(lambda x: CountTriangles(x)))			#REDUCE PHASE
+
+	#ROUND 2
+	#Summing number of triangles for each color
+	t = (rdd.map(lambda x: x[1])		#MAP PHASE
+		.reduce(lambda x,y: x+y))		#REDUCE PHASE
+
+	#Estimated number of triangles formed by the input edges
+	t_final = C**2 * t
+
+	return t_final
+
+
+
+#ALGORITHM 2
+#input:
+#		edges - RDD with the edges
+#		C - number of colors
+#output:
+#		t_final - estimate of the number of triangles
+def MR_ExactTC(edges, C):
+	p = 8191
+	a = random.randint(1, p-1)
+	b = random.randint(0, p-1)
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+	# CHECKING NUMBER OF CMD LINE PARAMETERS
+	assert len(sys.argv) == 5, "Usage: python G078HW2.py <C> <R> <F> <file_name>"
+
+	# SPARK SETUP
+	conf = SparkConf().setAppName('G078HW2').setMaster("local[*]")
+	sc = SparkContext(conf=conf)
+
+	# INPUT READING
+	# 1. Read number of colors
+	C = sys.argv[1]
+	assert C.isdigit(), "C must be an integer - C is the number of colours"
+	C = int(C)
+
+	# 2. Read number of runs
+	R = sys.argv[2]
+	assert R.isdigit(), "R must be an integer - R is the number of runs"
+	R = int(R)
+
+	# 3. Read flag
+	F = sys.argv[3]
+	assert F.isdigit(), "F must be a single digit binary (0 or 1) - F is the flag that switches between algorithm 1 and algorithm 2"
+	F = bin(F)
+
+	# 4. Read input file: in this case it'll be a .txt file
+	data_path = sys.argv[4]
+	assert os.path.isfile(data_path), "File or folder not found"
+	rawData = sc.textFile(data_path) 	#RDD of Strings
+	edges = rawData.map(lambda x: (int(x.split(",")[0]), int(x.split(",")[1]))).cache()		#RDD of integers
+	edges = edges.repartition(32)
+
+	#Printing general info
+	print("\nOUTPUT with parameters:", )
+	print("\nDataset =", data_path)
+	print("\nNumber of Edges =", edges.count())	
+	print("\nNumber of Colors =", C)
+	print("\nNumber of Repetitions =", R)
+
+	if(F==0):
+		#ALGORITHM 1 RUNS
+		print("\nApproximation of algorithm with node coloring")
+		results_alg1 = [0] * R 		#Results stored to compute median
+		runningTime_alg1 = [0] * R 		#Running time for run i
+		for i in range(R):
+			start = time.time() * 1000		#Starting time in milliseconds
+			results_alg1[i] = MR_ApproxTCwithNodeColors(edges, C)
+			stop = time.time() * 1000		#Stopping time in milliseconds
+			runningTime_alg1[i] = stop - start
+			#print("\tRUN", i, "-> Estimate of t:", results_alg1[i])
+
+		#Printing the median of R runs
+		results_alg1.sort()
+		if(R%2==1):
+			median_alg1 = results_alg1[int(R/2)]
+		else:
+			median_alg1 = (results_alg1[R/2] + results_alg1[R/2-1])/2
+
+		print("\n- Number of triangles (median over", R, "runs =", median_alg1)
+		print("\n- Running time (average over", R, "runs) =", sum(runningTime_alg1)/R, "ms")
+	
+	elif(F==1):
+		#ALGORITHM 2 RUN
+		print("\nMR_ExactTC:")
+		results_alg1 = [0] * R 		#Results stored to compute median
+		runningTime_alg1 = [0] * R 		#Running time for run i
+		for i in range(R):
+			start = time.time() * 1000		#Starting time in milliseconds
+			results_alg2[i] = MR_ExactTC(edges, C)
+			stop = time.time() * 1000		#Stopping time in milliseconds
+			runningTime_alg2[i] = stop - start
+			print("\tRUN", i, "-> Estimate of t:", results_alg1[i])	#Should be all equal
+		
+		print("\tEstimate of t:", result_alg2[R-1])
+		print("\n\tRUNNING TIME (ms):", runningTime_alg2)
+
+
+
+if __name__ == "__main__":
+	main()
+
+
+
+
